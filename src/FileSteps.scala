51966a4ce6a681131eef56a49bcd84d8067c057d
Niranjan Tallapalli <niranjan.tallapalli@gmail.com>
2018-06-15 16:59:28 -0500

    Initial checkin

---
+++ src/FileSteps.scala
@@ -0,0 +1,14 @@
+object FileSteps{
+  
+def loadFile(path: String,sep: String,header: boolean): Dataframe = {
+    val dataframe = spark
+                .read
+                .option(“sep”,”,”)
+                .option(“header” ,false)
+                .csv(path)  
+       
+     Dataframe
+} 
+
+  }
+
